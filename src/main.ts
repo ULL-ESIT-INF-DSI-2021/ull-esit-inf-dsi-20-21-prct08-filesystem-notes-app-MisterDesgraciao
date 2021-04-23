9cc341e895067c46849242b21adc1d48214d7bc0
Óscar Pozo Fernández <alu0101036526@ull.edu.es>
2021-04-23 11:29:25 +0100

    Continuando handler

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import {ListaNotasPrinter} from './listanotasprinter';
 import {ListaNotas} from './listanotas';
 import {Nota} from './nota';
 // import {Colores} from './nota';
-// import * as chalk from 'chalk';
-// import * as fs from 'fs';
+import * as chalk from 'chalk';
+import * as fs from 'fs';
 import * as yargs from 'yargs';
 
 const NotaDSI = new Nota('Óscar Pozo', 'DSI',
@@ -18,7 +18,60 @@ yargs.parse();
 
 yargs.command({
   command: 'add',
-  describe: 'Add a new note',
+  describe: 'Añadir una Nota nueva',
+  builder: {
+    usuario: {
+      describe: 'Usuario',
+      demandOption: true,
+      type: 'string',
+    },
+    titulo: {
+      describe: 'Título de la nota',
+      demandOption: true,
+      type: 'string',
+    },
+    cuerpo: {
+      describe: 'Cuerpo de la nota',
+      demandOption: true,
+      type: 'string',
+    },
+    color: {
+      describe: 'Color de la nota',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.usuario === 'string' &&
+        typeof argv.titulo === 'string' &&
+        typeof argv.cuerpo === 'string' &&
+        typeof argv.color === 'string') {
+      if (argv.color === 'Rojo' ||
+          argv.color === 'Verde' ||
+          argv.color === 'Azul' ||
+          argv.color === 'Amarillo') {
+        const nuevaNota = new Nota(
+            argv.usuario, argv.titulo, argv.cuerpo, argv.color);
+        NotasPC.addNota(nuevaNota);
+        if (!fs.existsSync(`./users/${argv.usuario}`)) {
+          console.log(chalk.red.inverse('No existe la carpeta.'));
+          console.log(chalk.green.inverse('Creando la carpeta.'));
+          fs.mkdirSync(`./users/${argv.usuario}`);
+        }
+        if (fs.existsSync(`./users/${argv.usuario}/${argv.titulo}`)) {
+          console.log(chalk.red.inverse('ERROR. El título ya existe.'));
+        } else {
+          console.log(chalk.green('Añadimos la nota.'));
+        }
+      }
+    }
+  },
+});
+
+/*
+yargs.command({
+  command: 'modify',
+  describe: 'Modificar una nota existente.',
   builder: {
     usuario: {
       describe: 'Usuario',
@@ -57,3 +110,4 @@ yargs.command({
     }
   },
 });
+*/
